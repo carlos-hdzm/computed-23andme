9948afeeff3802b2b414801134656cc28af5dfb3
Carlos <j_carlos.hdz@hotmail.com>
2026-08-08 21:01:23 -0700

    Test config changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,34 +1,39 @@
-import { defineConfig } from 'vitest/config'
-import { playwright } from '@vitest/browser-playwright'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import { playwright } from "@vitest/browser-playwright";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    browser: {
-      enabled: true,
-      provider: playwright(),
-      // https://vitest.dev/config/browser/playwright
-      instances: [
-        { browser: 'chromium' },
-      ],
-    },
     coverage: {
-      provider: 'v8',
+      provider: "v8",
     },
     projects: [
       {
+        extends: true,
         test: {
-          name: 'node',
-          include: ['src/data-processing/**/*.test.ts'],
+          name: "node",
+          include: ["src/data-processing/**/*.test.ts"],
+          environment: "node",
         },
       },
       {
+        extends: true,
         test: {
-          name: 'browser',
-          include: ['src/components/**/*.test.tsx', 'src/context/**/*.test.{ts|tsx}'],
+          name: "browser",
+          include: [
+            "src/components/**/*.test.tsx",
+            "src/context/**/*.test.{ts,tsx}",
+          ],
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            // https://vitest.dev/config/browser/playwright
+            instances: [{ browser: "chromium" }],
+            headless: true,
+          },
         },
       },
     ],
   },
-})
+});
